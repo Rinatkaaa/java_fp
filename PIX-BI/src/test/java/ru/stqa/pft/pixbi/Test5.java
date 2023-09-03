aa7b51893042052fa6c253ddbb26dd129cd00635
Rinatkaaa <tataria99@mail.ru>
2023-09-03 23:21:55 +0300

    Рефакторинг

--- PIX-BI/src/test/java/ru/stqa/pft/pixbi/Test5.java
+++ PIX-BI/src/test/java/ru/stqa/pft/pixbi/Test5.java
@@ -12,49 +12,56 @@ import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
 
 public class Test5 {
-  private WebDriver wd;
+    private WebDriver wd;
 
-  @BeforeMethod(alwaysRun = true)
-  public void setUp() throws Exception {
-    wd = new FirefoxDriver();
-    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    wd.get("https://dev.pixbi.ru/login");
-    wd.findElement(By.id("login_username")).clear();
-    wd.findElement(By.id("login_username")).sendKeys("rinatganiev");
-    wd.findElement(By.xpath("//div[@id='root']/section/section/main/div/div")).click();
-    wd.findElement(By.id("login_password")).click();
-    wd.findElement(By.id("login_password")).clear();
-    wd.findElement(By.id("login_password")).sendKeys("Tataria99@");
-    wd.findElement(By.xpath("//form[@id='login']/div[3]/div/div/div/div/div/button/span")).click();
-  }
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        wd.get("https://dev.pixbi.ru/login");
+        login("rinatganiev", "Tataria99@");
+    }
 
-  @Test
-  public void tests5() throws Exception {
+    private void login(String username, String password) {
+        wd.findElement(By.id("login_username")).clear();
+        wd.findElement(By.id("login_username")).sendKeys(username);
+        wd.findElement(By.xpath("//div[@id='root']/section/section/main/div/div")).click();
+        wd.findElement(By.id("login_password")).click();
+        wd.findElement(By.id("login_password")).clear();
+        wd.findElement(By.id("login_password")).sendKeys(password);
+        wd.findElement(By.xpath("//form[@id='login']/div[3]/div/div/div/div/div/button/span")).click();
+    }
+
+    @Test
+    public void tests5() throws Exception {
+      gotoBasePage();
+    }
+
+  private void gotoBasePage() {
     wd.findElement(By.xpath("//div[@id='root']/section/section/main/section/div[4]/div[2]/div/a/div/img")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | win_ser_1 | ]]
   }
 
   @AfterMethod(alwaysRun = true)
-  public void tearDown() throws Exception {
-    wd.quit();
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
+    public void tearDown() throws Exception {
+        wd.quit();
     }
-  }
 
-  private boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
+    private boolean isElementPresent(By by) {
+        try {
+            wd.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
     }
-  }
+
+    private boolean isAlertPresent() {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
 
 }
